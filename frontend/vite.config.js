81ba67f34418ae7edc2fb3eb46e006199de8967e
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-10-21 13:44:59 +0300

    some edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://mern-auth-httponly-cookie.onrender.com/',
-        changeOrigin: true,
-      },
-    },
-  },
 });
